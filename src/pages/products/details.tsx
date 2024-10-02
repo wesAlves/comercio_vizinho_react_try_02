8396d51e1c7d703e9ebe12edace85ea28a7a3ff6
Wes Benvindo <wal2.designer@gmail.com>
2024-10-02 15:11:45 +0100

    Product card detail

--- src/pages/products/details.tsx
+++ src/pages/products/details.tsx
@@ -2,6 +2,7 @@ import {PageTitleBar} from "../../components/pageTitleBar.tsx";
 import {useEffect, useState} from "react";
 import {useLocation, useParams} from "react-router-dom";
 import cv_api from "../../apis/cv_api.ts";
+import {Box, Card, CardContent, CardMedia, Typography} from "@mui/material";
 
 const ProductDetails = () => {
 
@@ -19,17 +20,51 @@ const ProductDetails = () => {
     return (
         <>
             <PageTitleBar title={`Detalhe do produto: ${product?.name}`} isDetail={true}/>
-            <div>
-                <div>Id: {product?.id}</div>
-                <div>Nome: {product?.name}</div>
-                <div>Descrição: {product?.description}</div>
-                <div>Tipo: {product?.goodsType}</div>
-                <div>Categoria: Categoria</div>
-                <div>Preço: {product?.price}</div>
-                <div>Nome do vendedor: {product?.user.name}</div>
-                <div>Telefone do vendedor: {product?.user.phoneNumber}</div>
-                <div>E-mail do vendedor: {product?.user.email}</div>
-            </div>
+            <Box sx={{p: "32px"}}>
+                <div>Controllers</div>
+                <Card sx={{mt: "32px", border: "1px solid #d9d9d9", borderRaidus: "16px", width: 1200, mx: "auto"}}>
+                    <Box sx={{display: "flex", alignItems:"center"}}>
+                        <CardMedia
+                            component={"img"} sx={{width: 600}}
+                            image="/images/bicycle.png"
+                            alt="Live from space album cover"
+
+                        />
+                        <CardContent>
+                            <Box sx={{color: "#d9d9d9"}}>Id: {product?.id}</Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Nome:</Typography> {product?.name}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Descrição:</Typography> {product?.description}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Tipo:</Typography> {product?.goodsType}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Preço:</Typography> {product?.price}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Venddido por:</Typography> {product?.user.name}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>Telefone:</Typography> {product?.user.phoneNumber}
+                            </Box>
+                            <Box sx={{fontSize: "1.6em"}}>
+                                <Typography component={"span"}
+                                            sx={{fontSize: '.8em'}}>E-mail:</Typography> {product?.user.email}
+                            </Box>
+                        </CardContent>
+
+                    </Box>
+                </Card>
+            </Box>
         </>
 
     )
